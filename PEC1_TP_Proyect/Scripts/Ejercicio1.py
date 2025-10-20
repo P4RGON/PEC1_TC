15bcad24209e28a8169824311f6aba923f150684
Jesús Pariente Gonzalez <j.pargon.2001@hotmail.com>
2025-10-20 19:49:26 +0200

    Add: ProyectV2 && Fix: Name directories

--- PEC1_TP_Proyect/Scripts/Ejercicio1.py
+++ PEC1_TP_Proyect/Scripts/Ejercicio1.py
@@ -1,6 +1,6 @@
 # Variables tipo float, almacenamos el peso y la altura
 # map(float,...) transforma los datos de string a float
-# .replace(",", ".") cambia la coma por un punto por si lo cabias a Español
+# .replace(",", ".") cambia la coma por un punto por si usas a Español
 pesoKg, aluraM = map(float, input("Ingrese el peso en kilogramos y la altura en metros separados por un espacio: ").replace(",", ".").split())
 
 # Definimos variable imcTotal tipo float para calcular el IMC con decimales
